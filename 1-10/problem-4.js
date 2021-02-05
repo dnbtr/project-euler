b75bfe65fa0539fe67f43143049b79b43627b920
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-04 23:09:56 -0300

    Problem 4 (still WiP)

--- 1-10/problem-4.js
+++ 1-10/problem-4.js
@@ -2,20 +2,35 @@ console.log(
   '-----\nA palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.\nFind the largest palindrome made from the product of two 3-digit numbers.\n-----\n'
 );
 
-const checkIfNumberIsPalindrome = (number) => {
-  let num = number;
-  let reverseNum = number;
+const reverseNumber = (num) => {
+  let inverseNum = [];
+  let temp = '';
+  let numLength = num.length;
 
-  // For loop para reverter o número
-  for (i = 0; i <= reverseNum.length - 1; i++) {
-    console.log();
+  num = num.split('');
+
+  for (i = 0; i <= numLength - 1; i++) {
+    temp = num.pop();
+    inverseNum.push(temp);
   }
+  return inverseNum.join(',').replace(/,/g, '');
+};
+
+const checkIfNumberIsPalindrome = (number) => {
+  let reverseNumber = reverseNumber(number);
+  let largestPalindromeFound;
+
+  if (number === reverseNumber) {
+    console.log(`Number ${number} is a palindrome`);
+    largestPalindromeFound = number;
+  }
+
   // ???
 
   // Final da função
-  console.log(
-    num === reverseNum ? 'is a palindrome' : "is NOT a palindrome"
-  );
+  // console.log(num === reverseNum ? 'is a palindrome' : 'is NOT a palindrome');
 };
 
 checkIfNumberIsPalindrome();
+
+reverseNumber('212443');
