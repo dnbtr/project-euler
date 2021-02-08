49bec61f278a030e5b0ee0b51c1c18d53fb445d7
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-08 14:17:32 -0300

    Problem 5 solved

---
+++ 1-10/problem-5.js
@@ -0,0 +1,36 @@
+console.log(
+  '-----\n2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.\nWhat is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?\n-----\n'
+);
+
+const findIfEvenlyDivisibleByEveryNumberInInterval = (
+  number,
+  lowerBound,
+  upperBound
+) => {
+  let iterator = lowerBound;
+
+  while (iterator <= upperBound) {
+    if (number % iterator !== 0) {
+      // console.log(`Breaking. ${number} not divisible evenly by ${iterator}.`);
+      break;
+    }
+
+    if (iterator === upperBound) {
+      console.log(
+        `${number} is evenly divisible by all numbers from ${lowerBound} to ${upperBound}`
+      );
+      break;
+    }
+
+    iterator++;
+  }
+};
+
+/* 
+  Resolução do problema, escolher um número-limite para calcular
+*/
+let maxNumber = 999999999;
+
+for (k = 1; k < maxNumber; k++) {
+  findIfEvenlyDivisibleByEveryNumberInInterval(k, 1, 20);
+}
