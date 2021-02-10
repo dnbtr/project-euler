c4f2de4e6b1b73c69adab08ffa1d92e161f449d8
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-10 15:46:19 -0300

    Problem 10 solution is functional / But algorithm is slow

--- 1-10/problem-10.js
+++ 1-10/problem-10.js
@@ -1,3 +1,37 @@
 console.log(
   '-----\nThe sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.\nFind the sum of all the primes below two million.\n-----\n'
 );
+
+// process.on('SIGINT', () => {
+//   console.log('asdasdasdasdasdasdasdasdas');
+//   process.kill(process.pid, 'SIGTERM');
+// });
+
+
+const findIfNumberIsPrime = (number) => {
+  for (i = number - 1; i > 1; i--) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  console.log(`Number ${number} is prime`);
+  return number;
+};
+
+let primeNumberSum = 0;
+
+// Beginning at 2 since 1 isn't a prime number
+let iterator = 2;
+
+console.time('time elapsed iterating');
+// This will take O(n) to finish...
+while (iterator < 2000) {
+  let prime = findIfNumberIsPrime(iterator);
+  iterator++;
+
+  if (prime !== false) {
+    primeNumberSum += prime;
+  }
+}
+console.timeEnd('time elapsed iterating');
+console.log(primeNumberSum);
