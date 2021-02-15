26e2119499bf198a93987e92ba8664cc1118937a
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-14 23:50:01 -0300

    Problem 16 solved

---
+++ 11-20/problem-16.py
@@ -0,0 +1,10 @@
+base = 2
+power = 1000
+problemInput = str(pow(base, power))
+
+answer = 0
+
+for num in problemInput:
+  answer += int(num);
+
+print("The sum of all digits in", base, "^", power, "is", answer)
