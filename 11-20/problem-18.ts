d991d95ca4b51f4226688d279c7632ec25a8853f
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-16 21:44:15 -0300

    Problem 18 (WiP)

--- 11-20/problem-18.ts
+++ 11-20/problem-18.ts
@@ -2,8 +2,7 @@ console.log(
   '-----\nBy starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.\n\n3\n7 4\n 2 4 6\n8 5 9 3\n\nThat is, 3 + 7 + 4 + 9 = 23.\nFind the maximum total from top to bottom of the triangle below:\nNOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)\n-----\n'
 );
 
-const triangle = `
-75
+let problemInput = `75
 95 64
 17 47 82
 18 35 87 10
@@ -19,3 +18,44 @@ const triangle = `
 63 66 04 68 89 53 67 30 73 16 69 87 40 31
 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
 `;
+
+type Triangle = Array<string>;
+
+const extractString = (inputString: string): string => {
+  return inputString.substring(0, inputString.indexOf('\n'));
+};
+
+const assembleTriangle = (input: string): Array<string> => {
+  let str: string;
+  let splitStr: any;
+  let strArr: Int8Array;
+  let triangle = [];
+
+  for (let i = 0; i < 15; i++) {
+    str = extractString(input);
+    splitStr = str.split(' ');
+
+    // console.log(splitStr);
+    strArr = new Int8Array([...splitStr]);
+    console.log(strArr);
+    triangle.push(strArr);
+
+    input = input.replace(input.substring(0, input.indexOf('\n') + 1), '');
+  }
+  return triangle;
+};
+
+const maximumPathSum = (triangle: Triangle): number => {
+  let sum: number;
+  let iterator: number = 0;
+  for (let i = 0; i < triangle.length; i++) {
+
+  }
+  return sum;
+}
+
+const main = (): void => {
+  const triangle = assembleTriangle(problemInput);
+};
+
+main();
