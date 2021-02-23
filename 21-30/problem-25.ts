5494f2a678ee0f95901300cc2c07b0b7298073af
dnbtr <daniel.bittar@alumni.usp.br>
2021-02-22 21:46:44 -0300

    refactor: problem 25

--- 21-30/problem-25.ts
+++ 21-30/problem-25.ts
@@ -1,6 +1,6 @@
 // Problem 25 - 1000-digit Fibonacci number
 
-let currentNumber: bigint = BigInt(0);
+let nextFibonacciNumber: bigint = BigInt(0);
 let fibonacciArray: bigint[] = new Array();
 let str: string = '';
 
@@ -9,17 +9,17 @@ fibonacciArray[0] = BigInt(0);
 fibonacciArray[1] = BigInt(1);
 fibonacciArray[2] = BigInt(1);
 
-const calcFirstFibWithNDigits = (numberOfDigits): void => {
+const calcFirstFibWithNDigits = (numberOfDigits: number): void => {
   console.time();
   for (let i = 2; i <= numberOfDigits * 1000; i++) {
-    currentNumber = BigInt(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
-    str = currentNumber.toString();
+    nextFibonacciNumber = BigInt(fibonacciArray[i - 1] + fibonacciArray[i - 2]);
+    str = nextFibonacciNumber.toString();
 
     if (str.length === numberOfDigits) {
       console.log(`Number Fib(${i}) has ${numberOfDigits} digits or more`);
       break;
     }
-    fibonacciArray[i] = currentNumber;
+    fibonacciArray[i] = nextFibonacciNumber;
   }
   console.timeEnd();
 }
