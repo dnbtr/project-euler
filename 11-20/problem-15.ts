bd1775d0769bf0e792cce86e033ca4f9366e1b4c
dnbtr <daniel.bittar@alumni.usp.br>
2021-03-16 02:09:53 -0300

    refactor: Problem 15 to Typescript

---
+++ 11-20/problem-15.ts
@@ -0,0 +1,33 @@
+/*
+
+Problem 15 - Lattice paths
+
+Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+there are exactly 6 routes to the bottom right corner.
+How many such routes are there through a 20×20 grid?
+*/
+
+/*
+  Notes
+
+  For a 2x2 grid, there are 3x3 routes possible:
+  [0,0] > [0,1] > [0,2] > [1,2] > [2,2]
+  ... etc
+
+  So for a N x N grid, N+1 x N+1 matrix is needed
+*/
+
+import { factorial } from '../utils';
+
+(() => {
+  const main = () => {
+    let gridRowSize = 20;
+    let gridColumnSize = 20;
+
+    let answer = factorial(gridRowSize + gridColumnSize) / factorial(gridRowSize) / factorial(gridColumnSize);
+    answer = Math.round(answer);
+
+    console.log(`For a ${gridRowSize} by ${gridColumnSize} grid, there are ${answer} possible paths;`);
+  };
+  main();
+})()
