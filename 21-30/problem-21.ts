82003cd3b70b132f74f177ed1f2f21dc0f052b75
dnbtr <daniel.bittar@alumni.usp.br>
2021-03-16 02:38:05 -0300

    refactor: Problem 21 due to changes in utils

--- 21-30/problem-21.ts
+++ 21-30/problem-21.ts
@@ -12,60 +12,64 @@
 
 import { findAndSumAllDivisors } from '../utils';
 
-interface amicableNumberInterface {
+interface amicableNumberObject {
   isAmicable: boolean;
   pair: number[] | null;
 }
 
-const isAmicableNumber = (inputNum: number): amicableNumberInterface => {
+(() => {
+  const isAmicableNumber = (inputNum: number): amicableNumberObject => {
 
-  let firstNum = findAndSumAllDivisors(inputNum);
-  let secondNum = findAndSumAllDivisors(firstNum);
+    let firstNum = findAndSumAllDivisors(inputNum);
+    let secondNum = findAndSumAllDivisors(firstNum);
 
-  // If amicable number pair was found
-  if (inputNum != firstNum && secondNum == inputNum) {
-    return {
-      isAmicable: true,
-      pair: [inputNum, firstNum]
-    }
-  } else {
-    return {
-      isAmicable: false,
-      pair: null
-    }
-  }
-}
-
-const findAmicableNumbersUnder = (maxLimit: number): Array<number> => {
-  let array: number[] = [];
-
-  for (let i = 0; i < maxLimit; i++) {
-
-    // If i was already calculated, skip this iteration;
-    if (array.includes(i)) continue;
-
-    let iterator = isAmicableNumber(i);
-
-    // If an amicable number pair was found
-    if (iterator.isAmicable) {
-      iterator.pair?.forEach(number => {
-        array.push(number);
-      });
+    // If amicable number pair was found
+    if (inputNum != firstNum && secondNum == inputNum) {
+      return {
+        isAmicable: true,
+        pair: [inputNum, firstNum]
+      }
+    } else {
+      return {
+        isAmicable: false,
+        pair: null
+      }
     }
   }
-  return array;
-}
 
-const main = (): void => {
-  const amicableNumbersArray = findAmicableNumbersUnder(10000);
-  let answer: number = 0;
+  const findAmicableNumbersUnder = (maxLimit: number): Array<number> => {
+    let array: number[] = [];
 
-  // Summing everything
-  amicableNumbersArray.forEach(number => {
-    answer += number;
-  });
+    for (let i = 0; i < maxLimit; i++) {
 
-  console.log(`The answer is ${answer}`);
-}
+      // If i was already calculated, skip this iteration;
+      if (array.includes(i)) continue;
 
-main();
+      let iterator = isAmicableNumber(i);
+      // console.log(iterator);
+
+      // If an amicable number pair was found
+      if (iterator.isAmicable) {
+        iterator.pair?.forEach(number => {
+          array.push(number);
+        });
+      }
+    }
+    return array;
+  }
+
+  const main = (): void => {
+    const amicableNumbersArray = findAmicableNumbersUnder(10000);
+    let answer: number = 0;
+
+    // Summing everything
+    amicableNumbersArray.forEach(number => {
+      answer += number;
+    });
+
+    console.log(`The answer is ${answer}`);
+  }
+  // console.time();
+  main();
+  // console.timeEnd();
+})()
