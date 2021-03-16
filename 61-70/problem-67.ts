01f5cdc583c4908af9f3887682464abcf1f20d5c
dnbtr <daniel.bittar@alumni.usp.br>
2021-03-16 00:30:13 -0300

    refactor: Problem 67 due to change in /data

--- 61-70/problem-67.ts
+++ 61-70/problem-67.ts
@@ -1,10 +1,21 @@
+/*
+  Problem 67 - Maximum path sum II
+
+  By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+  3
+  7 4
+  2 4 6
+  8 5 9 3
+
+  That is, 3 + 7 + 4 + 9 = 23.
+  Find the maximum total from top to bottom in triangle.txt (right click and Save Link/Target As), a 15K text file containing a triangle with one-hundred rows.
+  This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
+ */
+
+import { problem67Input } from '../data';
+
 (() => {
-  console.log(
-    '-----\nBy starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.\n\n3\n7 4\n2 4 6\n8 5 9 3\n\nThat is, 3 + 7 + 4 + 9 = 23.\nFind the maximum total from top to bottom in triangle.txt (right click and Save Link/Target As), a 15K text file containing a triangle with one-hundred rows.\nThis is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)\n-----\n'
-  );
-
-  const problemInput = require('../assets/problem67.input');
-
   type Triangle = Array<TriangleRow>;
   type TriangleRow = Array<number>;
 
@@ -49,7 +60,7 @@
   }
 
   const main = (): void => {
-    const triangle = assembleTriangle(problemInput);
+    const triangle = assembleTriangle(problem67Input);
     const answer = findMaximumPathSum(triangle);
     console.log('The answer is' + ' ' + answer);
   };
