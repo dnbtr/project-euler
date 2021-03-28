49235377624c3d82050d30aa931339981ffd54c0
dnbtr <daniel.bittar@alumni.usp.br>
2021-03-28 18:15:17 -0300

    refactor(utils): to ESLint rules / add rules to package.json / update packages

--- utils/index.ts
+++ utils/index.ts
@@ -3,98 +3,109 @@ interface CollatzSequenceObject {
   sequence: number[];
 }
 
-export const factorial = (number: number): number => {
+export function factorial(number: number): number {
   let result = 0;
-  if (number == 1) return 1;
-  if (number == 2) return 2;
+  if (number === 1) return 1;
+  if (number === 2) return 2;
 
   for (let i = number; i > 1; i--) {
-    if (i == number) result += i * (i - 1);
+    if (i === number) result += i * (i - 1);
     else result *= i - 1;
   }
   return result;
-};
+}
+
+export function permutation(n: number, k: number): number {
+  return factorial(n) / factorial(n - k);
+}
 
 // See https://betterexplained.com/articles/easy-permutations-and-combinations/
-export const combinationGeneral = (n: number, k: number): number => {
+export function combinationGeneral(n: number, k: number): number {
   return factorial(n) / (factorial(n - k) * factorial(k));
-};
+}
 
-export const combination = (n: number, k: number): number => {
+export function combination(n: number, k: number): number {
   return permutation(n, k) / factorial(k);
-};
+}
 
-export const permutation = (n: number, k: number): number => {
-  return factorial(n) / factorial(n - k);
-};
+export function isPrime(num: number): number | boolean {
+  if (num <= 1) return false;
 
-/* 
+  // 2 and 3 are both primes
+  if (num <= 3) return num;
+
+  // Research this property of primes... Every prime is odd, but what about %3?
+  if (num % 2 === 0 || num % 3 === 0) return false;
+
+  for (let i = 5; i * i <= num; i += 6) {
+    if (num % i === 0 || num % (i + 2) === 0) return false;
+  }
+  // console.log(`Number ${num} is prime`);
+  return num;
+}
+
+/*
   Takes O(sqrt(n)) to execute
 */
-export const findAllProperDivisors = (number: number): Array<number> => {
-  // console.log(`Calculating divisors of ${number}...`);
-  let divisors: Array<number> = [1];
+export function findAllProperDivisors(number: number): Array<number> {
+  const divisors: Array<number> = [1];
 
-  if (number == 1) return divisors;
+  if (number === 1) return divisors;
 
   for (let i = 2; i <= Math.sqrt(number); i++) {
-    if (number % i == 0) {
-      if (number / i == i || i == 1) divisors.push(i);
+    if (number % i === 0) {
+      if (number / i === i || i === 1) divisors.push(i);
       else {
         divisors.push(i);
         divisors.push(number / i);
       }
     }
   }
-  // console.log(`divisors for ${number}`, divisors);
-  return divisors
+  return divisors;
 }
 
-export const findAndSumAllProperDivisors = (number: number): number => {
-
-  let sum: number = findAllProperDivisors(number)
-    .reduce((total, number) => {
-      return total += number;
+export function findAndSumAllProperDivisors(number: number): number {
+  const sum: number = findAllProperDivisors(number)
+    .reduce((total, num) => {
+      return total + num;
     });
-
-  return sum
+  return sum;
 }
 
-/* 
+/*
   Takes O(n) to execute
 */
-export const findAllDivisorsLinear = (number: number): Array<number> => {
+export function findAllDivisorsLinear(number: number): Array<number> {
   // console.log(`Calculating divisors of ${number}...`);
-  let divisors: Array<number> = [];
+  const divisors: Array<number> = [];
 
   for (let i = 1; i < number; i++) {
-    if (number % i == 0) divisors.push(i);
+    if (number % i === 0) divisors.push(i);
   }
-  return divisors
+  return divisors;
 }
 
-/* 
+/*
   Takes O(n) to execute
 */
+export function findAndSumAllDivisorsLinear(number: number): number {
+  const divisors: Array<number> = [];
+  let sum = 0;
 
-export const findAndSumAllDivisorsLinear = (number: number): number => {
-  let divisors: Array<number> = [];
-  let sum: number = 0;
-
-  if (number == 1) return 1;
+  if (number === 1) return 1;
 
   for (let i = 1; i < number; i++) {
-    if (number % i == 0) divisors.push(i);
+    if (number % i === 0) divisors.push(i);
   }
-  divisors.forEach(number => {
-    sum += number;
+  divisors.forEach((num) => {
+    sum += num;
   });
-  return sum
+  return sum;
 }
 
-export const findAllPrimesSmallerThan = (limit: number): Array<number> => {
-  let iterator: number = 0;
-  let primeArray: number[] = [];
+export function findAllPrimesSmallerThan(limit: number): Array<number> {
+  const primeArray: number[] = [];
+  let iterator = 0;
 
   while (iterator < limit) {
     if (isPrime(iterator)) primeArray.push(iterator);
@@ -103,9 +114,9 @@ export const findAllPrimesSmallerThan = (limit: number): Array<number> => {
   return primeArray;
 }
 
-export const findNthPrime = (nthPrime: number): number => {
-  let iterator: number = 0;
-  let primeArray: number[] = [];
+export function findNthPrime(nthPrime: number): number {
+  const primeArray: number[] = [];
+  let iterator = 0;
 
   while (primeArray.length < nthPrime) {
     if (isPrime(iterator)) primeArray.push(iterator);
@@ -114,30 +125,29 @@ export const findNthPrime = (nthPrime: number): number => {
   return primeArray[nthPrime - 1];
 }
 
-export const findCollatzSequenceOf = (startNumber: number): CollatzSequenceObject => {
-  let sequence = [startNumber];
+export function findCollatzSequenceOf(startNumber: number): CollatzSequenceObject {
+  const sequence = [startNumber];
   let number = startNumber;
 
   while (number > 1) {
-    if (number % 2 == 0) number = number / 2;
+    if (number % 2 === 0) number /= 2;
     else number = 3 * number + 1;
     sequence.push(number);
   }
-
   return {
     number: startNumber,
-    sequence: sequence,
+    sequence,
   };
-};
+}
 
-export const findLongestCollatzSequenceUnder = (limit: number): CollatzSequenceObject => {
+export function findLongestCollatzSequenceUnder(limit: number): CollatzSequenceObject {
   let longestSequence: CollatzSequenceObject = {
     number: 0,
     sequence: [],
-  }
+  };
 
   for (let i = 1; i < limit; i++) {
-    let currentSequence = findCollatzSequenceOf(i);
+    const currentSequence = findCollatzSequenceOf(i);
     if (currentSequence.sequence.length > longestSequence.sequence.length) {
       longestSequence = currentSequence;
       console.log(`Longest Collatz sequence so far: ${longestSequence.sequence.length} terms, starting at number ${longestSequence.number}`);
@@ -146,30 +156,13 @@ export const findLongestCollatzSequenceUnder = (limit: number): CollatzSequenceO
   return longestSequence;
 }
 
-export const isPrime = (num: number): number | boolean => {
-  if (num <= 1) return false;
+export function isNumberPalindrome(input: number): number | boolean {
+  const num: string = input.toString();
+  const numArr: string[] = [...num];
+  const inverseNum: string[] = [];
 
-  // 2 and 3 are both primes
-  if (num <= 3) return num;
-
-  // Research this property of primes... Every prime is odd, but what about %3?
-  if (num % 2 == 0 || num % 3 == 0) return false;
-
-  for (let i = 5; i * i <= num; i = i + 6) {
-    if (num % i == 0 || num % (i + 2) == 0) return false;
-  }
-  // console.log(`Number ${num} is prime`);
-  return num;
-};
-
-export const isNumberPalindrome = (input: number): number | boolean => {
-
-  let num: string = input.toString();
-  let inverseNum: string[] = [];
-  let register: string = '';
-  let revNum: string = '';
-
-  let numArr: string[] = [...num];
+  let register = '';
+  let revNum = '';
 
   for (let k = 0; k <= num.length - 1; k++) {
     register = numArr.pop()!;
@@ -178,10 +171,9 @@ export const isNumberPalindrome = (input: number): number | boolean => {
 
   revNum = inverseNum.join(',').replace(/,/g, '');
 
-  if (num == revNum) return true;
-
+  if (num === revNum) return true;
   return false;
-};
+}
 
 export function polinomialPrimeFormula1One(n: number): number {
   // 0 <= N <= 39
