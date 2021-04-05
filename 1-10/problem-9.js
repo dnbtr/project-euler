cb8ae133edc865ddab51cb03317295f369ca96b0
dnbtr <daniel.bittar@alumni.usp.br>
2021-04-04 21:12:14 -0300

    Problem 9 (WiP)

--- 1-10/problem-9.js
+++
@@ -1,3 +0,0 @@
-console.log(
-  '-----\nA Pythagorean triplet is a set of three natural numbers, a < b < c, for which,\n a^2 + b^2 = c^2\nFor example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.\nThere exists exactly one Pythagorean triplet for which a + b + c = 1000.\nFind the product abc.\n-----\n'
-);
