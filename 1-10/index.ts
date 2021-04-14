bd99df4c26c90c2c5e9d5c218cb217e6bab2aa05
dnbtr <daniel.bittar@alumni.usp.br>
2021-04-14 00:20:11 -0300

    refactor(1-10): index.ts

--- 1-10/index.ts
+++ 1-10/index.ts
@@ -11,21 +11,29 @@ import problem8 from './problem-8';
 import problem9 from './problem-9';
 import problem10 from './problem-10';
 
-const projectEulerProblems1to10 = [
+const problems1To10 = [
   problem1, problem2, problem3, problem4, problem5,
   problem6, problem7, problem8, problem9, problem10,
 ];
-let counter = 0;
 
-const projectEulerProblems1to10Answers = [
+const problems1To10Answers = [
   233168, 4613732, 6857, 906609, 232792560,
-  25164150, 104743, 23514624000, null, 142913828922,
+  25164150, 104743, 23514624000, undefined, 142913828922,
 ];
 
-projectEulerProblems1to10.forEach((problem) => {
-  const currentProblemAsnwer = problem();
-  console.log(`problem ${counter + 1} answer = `, currentProblemAsnwer);
-  console.assert(currentProblemAsnwer === projectEulerProblems1to10Answers[counter]);
+let counter = 0;
+
+problems1To10.forEach((problem) => {
+  const currentProblem = counter + 1;
+  const actual = problem();
+  const expected = problems1To10Answers[counter];
+
+  console.log(`problem ${currentProblem} answer = `, actual);
+
+  try {
+    assert(actual === expected);
+  } catch (e) {
+    console.error(`\n---\nProblem ${currentProblem} failed.\nactual: ${actual}\nexpected: ${expected}\n---\n`);
+  }
   counter++;
-  console.log('\n');
 });
