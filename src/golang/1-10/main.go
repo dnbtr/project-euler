767acc9fb850dd02a5b260b38c6b43b9744bd010
dnbtr <daniel.bittar@alumni.usp.br>
2021-06-21 22:20:13 -0300

    feat(golang): problem 1 solved

--- src/golang/1-10/main.go
+++ src/golang/1-10/main.go
@@ -3,27 +3,45 @@ package main
 import "fmt"
 
 func main() {
-	const ARR_SIZE int = 1000
+	/*
+		The sum of all divisors of 0 >= number < 1000 can't be greater than 533
+		(1000/5) + (1000/3) = 533,3...
+	*/
+	const maxArraySize int = 533
 	var (
-		bufferArrSlice []int
-		bufferArr      [ARR_SIZE]int
+		answer                 int = 0
+		bufferArray            [maxArraySize]int
+		max_size               int = 0
+		mutiplesOfThreeandFive []int
 	)
 
-	bufferArr = findAllMultiplesOfThreeOrFiveUnder(ARR_SIZE, bufferArr)
-	bufferArrSlice = bufferArr[0:100]
+	bufferArray, max_size = findAllMultiplesOfThreeOrFiveUnder(maxArraySize, bufferArray)
+	mutiplesOfThreeandFive = bufferArray[0:max_size]
 
-	fmt.Println(bufferArrSlice)
+	for i := 0; i < len(mutiplesOfThreeandFive); i++ {
+		answer += mutiplesOfThreeandFive[i]
+	}
+	fmt.Println("The answer is:", answer)
 }
 
-func findAllMultiplesOfThreeOrFiveUnder(arrSize int, arr [1000]int) [1000]int {
-	var COUNTER int = 0
+func findAllMultiplesOfThreeOrFiveUnder(arrSize int, arr [533]int) ([533]int, int) {
+	var currentIndex int = 0
+	var max int = 0
 	var multiplesArray = arr
 
-	for i := 1; i < arrSize; i++ {
+	for i := 0; i < 1000; i++ {
+		// If i is 0, skip iteration
+		if i == 0 { continue }
+
+		// Else, check if divisible by 3 or 5
 		if i%3 == 0 || i%5 == 0 {
-			multiplesArray[COUNTER] = i
-			COUNTER++
+			multiplesArray[currentIndex] = i
+			currentIndex++
+			}
 		}
+
+	for i, number := range multiplesArray {
+		if number == 0 { max = i; break }
 	}
-	return multiplesArray
+	return multiplesArray, max
 }
