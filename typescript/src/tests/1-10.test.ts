f425ebe58925c62a229ebf702df741d8d90a502b
dnbtr <daniel.bittar@alumni.usp.br>
2021-06-28 01:12:54 -0300

    fix/tests: add the missing unit tests
    .gitignore was ignoring the tests

---
+++ typescript/src/tests/1-10.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import {
+  answers, problem1, problem2, problem3, problem4, problem5, problem6, problem7, problem8, problem9, problem10,
+} from '../1-10'
+
+describe('Problems 1 to 10', () => {
+  it('problem 1', () => { const result = problem1(); expect(result).to.be.equal(answers[0]) })
+
+  it('problem 2', () => { const result = problem2(); expect(result).to.be.equal(answers[1]) })
+
+  it('problem 3', () => { const result = problem3(); expect(result).to.be.equal(answers[2]) })
+
+  it('problem 4', () => { const result = problem4(); expect(result).to.be.equal(answers[3]) })
+
+  it('problem 5', () => { const result = problem5(); expect(result).to.be.equal(answers[4]) })
+
+  it('problem 6', () => { const result = problem6(); expect(result).to.be.equal(answers[5]) })
+
+  it('problem 7', () => { const result = problem7(); expect(result).to.be.equal(answers[6]) })
+
+  it('problem 8', () => { const result = problem8(); expect(result).to.be.equal(answers[7]) })
+
+  it('problem 9', () => { const result = problem9(); expect(result).to.be.equal(answers[8]) })
+
+  it('problem 10', () => { const result = problem10(); expect(result).to.be.equal(answers[9]) })
+})
---
+++ typescript/src/tests/11-20.test.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import {
+  answers, problem11, problem12, problem13, problem14, problem15, problem16, problem17, problem18, problem19, problem20,
+} from '../11-20'
+
+describe('Problems 11 to 20', () => {
+  it('problem 11', () => { const result = problem11(); expect(result).to.be.equal(answers[0]) })
+
+  it('problem 12', () => { const result = problem12(); expect(result).to.be.equal(answers[1]) })
+
+  it('problem 13', () => { const result = problem13(); expect(result).to.be.equal(answers[2]) })
+
+  it('problem 14', () => { const result = problem14(); expect(result).to.be.equal(answers[3]) })
+
+  it('problem 15', () => { const result = problem15(); expect(result).to.be.equal(answers[4]) })
+
+  it('problem 16', () => { const result = problem16(); expect(result).to.be.equal(answers[5]) })
+
+  it('problem 17', () => { const result = problem17(); expect(result).to.be.equal(answers[6]) })
+
+  it('problem 18', () => { const result = problem18(); expect(result).to.be.equal(answers[7]) })
+
+  it('problem 19', () => { const result = problem19(); expect(result).to.be.equal(answers[8]) })
+
+  it('problem 20', () => { const result = problem20(); expect(result).to.be.equal(answers[9]) })
+})
