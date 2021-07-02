27a752700baa31e0c12c548d3dfa5fd452d53ef1
dnbtr <daniel.bittar@alumni.usp.br>
2021-07-02 02:06:10 -0300

    refactor(typescript/tests)(WiP): simplify 1-10 tests
    Tests passing, but problems to track the time of each test
    
    Probably due to Mocha internals and how functions and 'it' statements are hoisted

--- typescript/src/_tests/1-10.test.ts
+++ typescript/src/_tests/1-10.test.ts
@@ -24,24 +24,34 @@ describe('Problems 1 to 10', () => {
 
   it('problem 10', () => { const result = problem10(); expect(result).to.be.equal(answers[9]) })
 })
-
 /*
 const problemsArray = [problem1, problem2, problem3, problem4, problem5, problem6, problem7, problem8, problem9, problem10]
 
+// It works, but requires further investigation
+// The tests are passing too quickly (< 1ms), problem 4 should be ~500, 600ms
+// The total suite run time is similar though (~4.4 secs)
+// But each it is apparently hoisted and executed after the whole describe function
+// So is there a solution to log each test elapsed time?
+
+console.time()
 describe('Problems 1 to 10', () => {
   let CURRENT_PROBLEM = 1
-  let ITERATOR = 0
 
-  problemsArray.forEach((problem) => {
-    // If CURRENT_PROBLEM has an answer (if i already solved that problem)
-    if (answers[ITERATOR] !== undefined) {
+  problemsArray.forEach(async (problem, index) => {
+    // console.log('forEach CURRENT_PROBLEM value', CURRENT_PROBLEM)
+
+    const result = problem()
+    // console.log(result)
+    // console.log(index)
+
+    if (answers[index] !== undefined) {
       it(`problem ${CURRENT_PROBLEM}`, () => {
-        const result: () => number = problemsArray[ITERATOR].prototype()
-        expect(result).to.be.equal(answers[ITERATOR])
-      })
+        // console.log('result:', result, ', index:', index, ', answers[index]:', answers[index])
+        expect(result).to.be.equal(answers[index])
+      }).timeout(0)
     }
     CURRENT_PROBLEM++
-    ITERATOR++
   })
 })
+console.timeEnd()
  */
